68845a137ed09a22574c7caf59642dafae3546b5
johaninfante747-droid <johaninfante747@gmail.com>
2026-03-13 10:27:18 -0400

    Adición del dataset al proyecto y actualización de otros archivos
    adición del Dataset/16. Retail-Dataset_real.csv y eliminación de archivos obsoletos (Dataset/video_games_sales.csv, informe.Rmd). Actualización de Latex/app.py and README.md, y renombramiento de "bibliografia.bib" a bibliografia(plantilla - por cambiar).bib.

--- Latex/app.py
+++ Latex/app.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 140 # Mejora la nitidez del gráfico
 plt.style.use('ggplot')          # Le da un estilo más profesional (parecido a R)
 
-df= pd.read_csv("Dataset/video_games_sales.csv")
+df= pd.read_csv("Dataset/16. Retail-Dataset_real.csv")
 
 print (df.head())
